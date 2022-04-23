d5878b72250b2e51eecf5dbaa480b30a6527c868
Hugo Silva <hugo.silva@arquivei.com.br>
2022-04-23 00:22:04 -0300

    feat: :sparkles: add routes

---
+++ src/routes/AppRouter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Router, Switch, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import HomePage from '../components/pages/HomePage';
+import QuizPage from '../components/pages/QuizPage';
+import ResultsPage from '../components/pages/ResultsPage';
+
+export const history = createBrowserHistory();
+
+export default function AppRouter() {
+  return (
+    <Router history={history}>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/quizpage" exact component={QuizPage} />
+        <Route path="/resultspage" exact component={ResultsPage} />
+      </Switch>
+    </Router>
+  );
+}
