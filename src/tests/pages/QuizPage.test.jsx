8050f71b21f7454662f2cfbd5ecdd1634591f176
Hugo Silva <hugo.silva@arquivei.com.br>
2022-04-23 00:25:55 -0300

    test: :white_check_mark: add tests

---
+++ src/tests/pages/QuizPage.test.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import QuizPage from '../../components/pages/QuizPage';
+import { TriviaGameContext } from '../../context/TriviaGameContext';
+import TriviaGameManager from '../../context/TriviaGameManager';
+import { data } from '../__mock__/data';
+
+describe('View QuizPage correctly', () => {
+  const triviaGameManager = new TriviaGameManager('easy');
+  triviaGameManager.setQuestions(data.results);
+
+  it('should have 3 buttons', () => {
+    const { queryAllByTestId } = render(
+      <TriviaGameContext.Provider value={triviaGameManager}>
+        <QuizPage />
+      </TriviaGameContext.Provider>,
+    );
+
+    expect(queryAllByTestId('button')).toHaveLength(3);
+    expect(queryAllByTestId('button')[0]).not.toHaveAttribute('disabled');
+    expect(queryAllByTestId('button')[1]).not.toHaveAttribute('disabled');
+  });
+
+  it('should not have timer', () => {
+    const { queryAllByTestId } = render(
+      <TriviaGameContext.Provider value={triviaGameManager}>
+        <QuizPage />
+      </TriviaGameContext.Provider>,
+    );
+
+    expect(queryAllByTestId('timer')).toHaveLength(0);
+  });
+});
